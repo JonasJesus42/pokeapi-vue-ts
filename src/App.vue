10b63b395e23886b27abf3c28697bd166e52113b
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-24 13:10:36 -0300

    importando Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,11 @@
 <template>
-  <div id="nav">
-    <div class="img-area">
-      <img src="./assets/logo.png" alt="Logo">
-    </div>
-    <div>
-      <router-link to="/" class="link">Home</router-link>
-      <router-link to="/team" class="link">Team</router-link>
-    </div>
-  </div>
- <!--  <router-view /> -->
+  <Navbar />
 </template>
 
-<script lang="ts">
-import NavBar from './components/NavBar.vue'
-import store from '@/store';
-import { getPokemon } from '@/services/index'
-import Pokemons from '@/interfaces/pokemons'
+<script lang="ts" setup>
+import Navbar from './Navbar.vue'
 
-export default {
-  components: {
-    NavBar,
-  },
-  methods: {
-    async pokemon() {
-      if(store.state.pokemons.length < 150){
-        console.log(`entrou na função pokemon com ${store.state.pokemons.length}`)
-        for (let i = 1; i <= 150; i++) {
-          const objectPokemon = await getPokemon(i)
-          this.newObjectPokemon(objectPokemon)
-        }
-      }
-    },
-    newObjectPokemon(pokemonObj: Pokemons) {
-      const { id, name, types } = pokemonObj
-      const type = types.map(info => info.type.name).join(' | ')
-      const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-      const objectPokemon = {
-        id: id,
-        name: name,
-        type: type,
-        image: image,
-      }
-      store.commit('addInPokemons', objectPokemon)
-    },
-  },
-  mounted(){
-    this.pokemon()
-  },
-}
 </script>
-
 
 <style>
 body {
@@ -64,34 +20,6 @@ body {
   padding: 0;
   box-sizing: border-box;
 }
-
-/* start nav style */
-#nav {
-  width: 97.4vw;
-  height: 75px;
-  background-color: #303030;
-  display: flex;
-  padding: 0 10px 0 10px;
-  justify-content: space-between;
-  text-align: center;
-  align-items: center;
-}
-
-.link {
-  list-style: none;
-  text-decoration: none;
-  color: #d5f3d7;
-  font-family: monospace;
-  font-size: 1.5rem;
-  margin-left: 20px;
-}
-
-#nav img {
-  width: 50%;
-  height: 100%;
-}
-
-/* end nav style */
 /* start cads */
 .componente {
   margin: auto;
