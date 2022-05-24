cd007ec35bb24662f5810f2a95494cd9eebccbb2
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-24 15:34:27 -0300

    adicionando o a função allPokemon

--- src/services/metods.ts
+++ src/services/metods.ts
@@ -1,14 +1,23 @@
 import axios from "./index";
 
-export const pokelist = async () => {
-    const arrayPokemons: Array<object> = []
+export const pokeList = async () => {
+    let arrayPokemons: Array<object> = []
     try {
         let response = await axios.getAllPokemons();
-       let pokemonsData = response.data.results
-       arrayPokemons.push({...pokemonsData})
-
+        let pokemonsData = response.data.results
+        arrayPokemons = pokemonsData
     } catch (e) {
         console.error(e);
     }
     return arrayPokemons
 }
+
+
+export const allPokemon = async () => {
+    const pokeArray = []
+    for (let i = 1; i <= 150; i++) {
+        const objectPokemon = await axios.onlyPokemon(i)
+        pokeArray.push(objectPokemon.data)
+    }
+    return pokeArray
+  }
