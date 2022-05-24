ba2cda47e12f39e61f5b79d4fd787e7269bedd08
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-24 12:11:24 -0300

    adicionando metodo getAllPokemons no index do axios

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,4 +4,10 @@ const api = axios.create({
     baseURL:  'https://pokeapi.co/api/v2/'
 })
 
-export default api
+
+
+export default {
+    getAllPokemons() {
+        return api.get('pokemon?limit=150')
+    }
+}
