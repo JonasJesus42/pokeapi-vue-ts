75e1063c8b3d3b5ba73ccc63888fe9c136879b8d
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-25 10:15:29 -0300

    refactor: removing comments and function error

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,21 +19,14 @@ const store = new Vuex.Store<State>({
         /* criar as gets e sets*/
         addInPokemons(state, objectPokemon) {
             state.pokemons.push(objectPokemon)
-            console.log(`pokemon adicionado em pokemons`)
         },
         addPokemonInMyTeam(state, id) {
             const poke = store.state.pokemons[id - 1]
             state.myTeam.push({ ...poke })
-            console.log(`pokemon adicionado no time`)
         },
         remuvePokemon(state, id) {
             state.myTeam.splice(id, 1)
-            console.log(`pokemon foi removido`)
         },
-        setNamePokemon(state, { name, index }) {
-            state.myTeam[index].name = name
-            console.log(`Nome trocado`)
-        }
     },
     plugins: [vuexLocal.plugin]
 })
