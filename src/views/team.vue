05d4156a2aa389143d280ee786d477bdd10d0587
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-25 15:08:57 -0300

    adding styling and input for name change

--- src/views/team.vue
+++ src/views/team.vue
@@ -1,48 +1,51 @@
 <template>
-    <section class="my-team center ">
-      <div class="header-myteam">
-        <h2>You Team</h2>
-      </div>
-      <div class="containe">
-        <NewCardPokemon />
-      </div>
+    <section class="my-team center">
+        <div class="header-myteam">
+            <h2>You Team</h2>
+        </div>
+        <div class="containe" >
+            <NewCardPokemon v-for="pokemon in store.state.myTeam" :pokemon="pokemon"/>
+        </div>
     </section>
 </template>
-<script lang="ts">
-import NewCardPokemon from "../components/NewCardPokemon.vue";
-    export default{
-    components: { NewCardPokemon }
-}
+
+<script lang="ts" setup>
+import NewCardPokemon from '@/components/NewCardPokemon.vue'
+import store from '@/store';
+
+
+
 </script>
 
 <style scoped>
 .my-team {
-  min-height: 200px;
-  margin: 20px;
-  background-color: #FFFA77;
-  border-radius: 20px;
-  box-shadow: 0 0px 10px rgba(0, 0, 0, 0.5);
+    min-height: 200px;
+    margin: 20px;
+    background-color: #FFFA77;
+    border-radius: 20px;
+    box-shadow: 0 0px 10px rgba(0, 0, 0, 0.5);
 }
-.header-myteam{ 
-  background-color: #ffff;
-  border-radius: 20px 20px 0px 0px;
-  display: flex;
-  justify-content: center;
-  font-family: 'Lato', sans-serif;
-  font-size: 1.5rem;
-  color: #FFFA77;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
-  text-shadow: 0 0px 5px rgba(0, 0, 0, 0.7);
-  flex-wrap: wrap;
+
+.header-myteam {
+    background-color: #ffff;
+    border-radius: 20px 20px 0px 0px;
+    display: flex;
+    justify-content: center;
+    font-family: 'Lato', sans-serif;
+    font-size: 1.5rem;
+    color: #FFFA77;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
+    text-shadow: 0 0px 5px rgba(0, 0, 0, 0.7);
 }
-.containe{
-  display: flex;
-  box-sizing: border-box;
-  margin: 20px 0;
-  justify-content: center;
+
+.containe {
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
 }
+
 .center {
-  align-items: center;
-  justify-content: center;
+    align-items: center;
+    justify-content: center;
 }
 </style>
