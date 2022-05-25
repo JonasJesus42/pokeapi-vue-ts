263febbf25fd576190a6e2c1a261c7ea9bef70d8
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-25 13:26:47 -0300

    refactor: new html and css

--- src/views/team.vue
+++ src/views/team.vue
@@ -1,9 +1,11 @@
 <template>
-    <section class="title">
-        <h1>You Team</h1>
-    </section>
-    <section class="team">
+    <section class="my-team center ">
+      <div class="header-myteam">
+        <h2>You Team</h2>
+      </div>
+      <div class="containe">
         <NewCardPokemon />
+      </div>
     </section>
 </template>
 <script lang="ts">
@@ -14,9 +16,32 @@ import NewCardPokemon from "../components/NewCardPokemon.vue";
 </script>
 
 <style scoped>
-.team{
-    display: flex;
-    height: 80vh;
-    width: 98vw;
+.my-team {
+  min-height: 200px;
+  margin: 20px;
+  background-color: #FFFA77;
+  border-radius: 20px;
+  box-shadow: 0 0px 10px rgba(0, 0, 0, 0.5);
+}
+.header-myteam{ 
+  background-color: #ffff;
+  border-radius: 20px 20px 0px 0px;
+  display: flex;
+  justify-content: center;
+  font-family: 'Lato', sans-serif;
+  font-size: 1.5rem;
+  color: #FFFA77;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
+  text-shadow: 0 0px 5px rgba(0, 0, 0, 0.7);
+}
+.containe{
+  display: flex;
+  box-sizing: border-box;
+  margin: 20px 0;
+  justify-content: center;
+}
+.center {
+  align-items: center;
+  justify-content: center;
 }
 </style>
