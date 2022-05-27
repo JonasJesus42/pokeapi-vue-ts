77ce095a9f67d7580689d35d61eab72608257eb7
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-27 17:48:56 -0300

    refactor: i remuve useless css and changed the section class

--- src/components/Pokelist.vue
+++ src/components/Pokelist.vue
@@ -1,36 +1,23 @@
 <template>
-    <section className="container">
-        <Card v-for="(pokemon, i) in store.state.pokemons"
-           :id="i"
-           :pokemons="pokemon"
+    <section class="pokelist">
+        <Card v-for="(pokemon, index) in store.state.pokemons"
+           :index="index"
+           :obj="pokemon"
         />
     </section>
 </template>
 
 <script lang="ts" setup>
-import store from '@/store';
+import { store } from '@/store';
 import Card from './Card.vue';
-
-
 </script>
 
 <style scoped>
-section {
+.pokelist {
     box-sizing: border-box;
     height: 100%;
     width: 100%;
     flex-wrap: wrap;
     display: flex;
-}
-.header-myteam{ 
-  background-color: #ffff;
-  border-radius: 20px 20px 0px 0px;
-  display: flex;
-  justify-content: center;
-  font-family: 'Lato', sans-serif;
-  font-size: 1.5rem;
-  color: #FFFA77;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
-  text-shadow: 0 0px 5px rgba(0, 0, 0, 0.7);
 }
 </style>
