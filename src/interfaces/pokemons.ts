ca64efdd3062f997513880399a1d865825459954
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-05-27 17:53:25 -0300

    fix: add interface Pokemom, Sprite OtherPokemonsSprites, DreamWorld, PokemonType, NamedAPIResouce, PokemonAbility, PokemonStat

--- src/interfaces/pokemons.ts
+++ src/interfaces/pokemons.ts
@@ -1,17 +1,50 @@
-export interface Pokemons{
-    id?: number;
+export interface Pokemons {
+    id: number;
+    name: string;
+    types: PokemonType[];
+    sprites: Sprite;
+    index?: number;
+    abilities: PokemonAbility[];
+    stats: PokemonStat[];
+}
+export interface Pokemon{
     name?: string;
-    type?: string;
-    image?: string;
-    types?: object;
-    sprites?: Sprite;
     url?: string;
-    index?: number
+}
+interface Sprite {
+ front_default: string;
+ front_shiny: string;
+ front_female: string;
+ front_shiny_female: string;
+ back_default: string;
+ back_shiny: string;
+ back_female: string;
+ back_shiny_female: string;
+ other: OtherPokemonSprites;
+}
+interface OtherPokemonSprites {
+    dream_world: DreamWorld;
+}
+interface DreamWorld {
+    front_default: string;
+    front_female: string;
+}
+interface PokemonType {
+    slot: number;
+    type: NamedAPIResource;
+}
+interface NamedAPIResource {
+    name: string;
+    url: string;
+}
+interface PokemonAbility {
+    is_hidden: boolean;
+    slot: number;
+    ability: NamedAPIResource;
 }
 
-interface Sprite {
-    back_default: string;
-    back_shiny: string;
-    front_default: string;
-    front_shiny: string;
-}
+interface PokemonStat {
+    stat: NamedAPIResource;
+    effort: number;
+    base_stat: number;
+  }
